b3549e59b79bb83da531c7dffa32e97937a141bf
Sachintha Lasitha Viraj <sachinthaviraj@gmail.com>
2018-08-31 19:00:12 +0530

    NSBM Web Remake

---
+++ contact.php
@@ -0,0 +1,25 @@
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$message = $_POST['massage'];
+
+
+$email_from = "sachinthaviraj@outlook.com";
+
+$email_subject = "New Form Submission";
+
+$email_body = "User Name: $name.\n".
+	"User Email : $visitor_email.\n".
+	"User Message : $message.\n ";
+
+$to = "sachinthaviraj@gmail.com";
+
+$headers = "From: $email_from \r\n";
+
+$headers .="Reply-To: $visitor_email \r\n";
+
+mail($to,$email_subject,$email_body,$headers);
+
+header("Location: contact.html");
+
+?>
